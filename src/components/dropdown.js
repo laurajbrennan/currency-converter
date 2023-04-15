d6d67284931477457c7a60f00a583eacf84a384e
ellejaiebaie <48771627+laurajbrennan@users.noreply.github.com>
2023-04-15 11:23:47 -0700

    trying a different approach after this

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,43 +1,48 @@
 import React, { Component } from "react";
+// import { createRoot } from 'react-dom/client';
 // https://restcountries.com/v3.1/all?fields=name,currencies
 
 // https://v6.exchangerate-api.com/v6/080ff539a713154eccfbfd71/latest/USD
 
 export default class Dropdown extends Component {
-  state = {
-    loading: false,
-    countries: this.props.countries
-  };
-
-//   makeList = e => {
-//     e.preventDefault();
-//     this.setState({ loading: true });
-
-//   }
-
   render() {
     async function fetchCountries(endpoint) {
-        // this.setState({ loading: true });
         const res = await fetch(endpoint);
         const data = await res.json();
         return data;
-    }
-
-    fetchCountries('https://restcountries.com/v3.1/all?fields=name,currencies')
-    .then(data => {
-        console.log(data);
-        // this.setState({ countries: data, loading: false });
+    };
+    function makeList(array) {
+        array.map(entry => {
+            const currCode = entry[0];
+            const currName = entry[1];
+            return (
+                <option value={currCode}>{currCode} - {currName}</option>
+            )
+        })
+    };
+    // function makeList(array){
+    //     array.forEach((currency) => {
+    //         console.log(currency);
+    //         const currCode = currency[0];
+    //         const currName = currency[1];
+    //         const option = React.createElement("option", null, {currCode} - {currName});
+    //         addToPage(option);
+    //     })
+    // }
+    function start() {
+        fetchCountries('https://v6.exchangerate-api.com/v6/080ff539a713154eccfbfd71/codes').then(data => {
+        // console.log(data.supported_codes);
+        makeList(data.supported_codes);
     });
+    }
 
     return (
       <div>
         <div className="currency__input">
             <label htmlFor="cars">Choose a currency:</label>
-            <select name="currency">
-                <option value="volvo">Volvo</option>
-                <option value="saab">Saab</option>
-                <option value="fiat">Fiat</option>
-                <option value="audi">Audi</option>
+            <select name="currency" id="currency__select">
+                <option value="">Choose your currency</option>
+                
             </select>
         </div>  
       </div>
